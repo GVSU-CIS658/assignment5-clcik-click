5cf704b0549b3303c4c7fce1286dbd2dd8f3eac9
Hoan Lam <lamho@mail.gvsu.edu>
2026-04-02 01:23:28 +0000

    Work on everything

--- src/firebase.ts
+++ src/firebase.ts
@@ -3,12 +3,13 @@ import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
   //   // COPY this from your Firebase Console
-  //   apiKey: "your-api-key-goes-here",
-  //   authDomain: "your-project-name-here.firebaseapp.com",
-  //   databaseURL: "https://your-project-name-here.firebaseio.com",
-  //   projectId: "your-project-name-here",
-  //   storageBucket: "your-project-name.appspot.com",
-  //   messagingSenderId: "xxxxxxxx",
+  apiKey: "AIzaSyBf4sc0_QurLjNrGM2EI6OxMtD9bG3Bn-g",
+  authDomain: "beverageshop-ec537.firebaseapp.com",
+  projectId: "beverageshop-ec537",
+  storageBucket: "beverageshop-ec537.firebasestorage.app",
+  messagingSenderId: "337470522840",
+  appId: "1:337470522840:web:148b1e9931052bc0f65ead",
+  measurementId: "G-BEYPC7PY1G"
 };
 
 const app = initializeApp(firebaseConfig);
